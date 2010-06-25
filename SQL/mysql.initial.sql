5c001f907759273bde77f54f18574a2ac2f33281
hunterhdolan <hunterhdolan@6ef99c1f-ab0f-4b48-a075-740b4f2819d0>
2010-06-25 16:57:41 +0000

    Fixed SVN Error. Thinking about switching to GIT!

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -1,146 +1,279 @@
--- RoundCube Webmail initial database structure
+--
+--Crystal Mail MYSQL Installation Script
+--100% GNU GPL 3.0
+--
 
 
-/*!40014  SET FOREIGN_KEY_CHECKS=0 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
--- Table structure for table `session`
+--
+-- Table structure for table `accounts`
+--
 
-CREATE TABLE `session` (
- `sess_id` varchar(40) NOT NULL,
- `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `ip` varchar(40) NOT NULL,
- `vars` mediumtext NOT NULL,
- PRIMARY KEY(`sess_id`),
- INDEX `changed_index` (`changed`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `aid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_dn` varchar(128) NOT NULL,
+  `account_id` varchar(128) NOT NULL,
+  `account_pw` varchar(128) NOT NULL,
+  `account_host` varchar(128) NOT NULL,
+  `preferences` text,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`aid`),
+  KEY `user_id_fk_accounts` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-
--- Table structure for table `users`
-
-CREATE TABLE `users` (
- `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `username` varchar(128) NOT NULL,
- `mail_host` varchar(128) NOT NULL,
- `alias` varchar(128) NOT NULL,
- `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `last_login` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `language` varchar(5),
- `preferences` text,
- PRIMARY KEY(`user_id`),
- INDEX `username_index` (`username`),
- INDEX `alias_index` (`alias`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+--
+-- Dumping data for table `accounts`
+--
 
 
--- Table structure for table `messages`
+-- --------------------------------------------------------
 
-CREATE TABLE `messages` (
- `message_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
- `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
- `del` tinyint(1) NOT NULL DEFAULT '0',
- `cache_key` varchar(128) /*!40101 CHARACTER SET ascii COLLATE ascii_general_ci */ NOT NULL,
- `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `idx` int(11) UNSIGNED NOT NULL DEFAULT '0',
- `uid` int(11) UNSIGNED NOT NULL DEFAULT '0',
- `subject` varchar(255) NOT NULL,
- `from` varchar(255) NOT NULL,
- `to` varchar(255) NOT NULL,
- `cc` varchar(255) NOT NULL,
- `date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `size` int(11) UNSIGNED NOT NULL DEFAULT '0',
- `headers` text NOT NULL,
- `structure` text,
- PRIMARY KEY(`message_id`),
- CONSTRAINT `user_id_fk_messages` FOREIGN KEY (`user_id`)
-   REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
- INDEX `created_index` (`created`),
- INDEX `index_index` (`user_id`, `cache_key`, `idx`),
- UNIQUE `uniqueness` (`user_id`, `cache_key`, `uid`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
-
-
+--
 -- Table structure for table `cache`
+--
 
-CREATE TABLE `cache` (
- `cache_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `cache_key` varchar(128) /*!40101 CHARACTER SET ascii COLLATE ascii_general_ci */ NOT NULL ,
- `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `data` longtext NOT NULL,
- `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
- PRIMARY KEY(`cache_id`),
- CONSTRAINT `user_id_fk_cache` FOREIGN KEY (`user_id`)
-   REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
- INDEX `created_index` (`created`),
- INDEX `user_cache_index` (`user_id`,`cache_key`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+CREATE TABLE IF NOT EXISTS `cache` (
+  `cache_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cache_key` varchar(128) CHARACTER SET ascii NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `data` longtext NOT NULL,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`cache_id`),
+  KEY `created_index` (`created`),
+  KEY `user_cache_index` (`user_id`,`cache_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `cache`
+--
 
 
--- Table structure for table `contacts`
+-- --------------------------------------------------------
 
-CREATE TABLE `contacts` (
- `contact_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `del` tinyint(1) NOT NULL DEFAULT '0',
- `name` varchar(128) NOT NULL DEFAULT '',
- `email` varchar(128) NOT NULL,
- `firstname` varchar(128) NOT NULL DEFAULT '',
- `surname` varchar(128) NOT NULL DEFAULT '',
- `vcard` text NULL,
- `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
- PRIMARY KEY(`contact_id`),
- CONSTRAINT `user_id_fk_contacts` FOREIGN KEY (`user_id`)
-   REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
- INDEX `user_contacts_index` (`user_id`,`email`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+--
+-- Table structure for table `contactgroupmembers`
+--
 
+CREATE TABLE IF NOT EXISTS `contactgroupmembers` (
+  `contactgroup_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`contactgroup_id`,`contact_id`),
+  KEY `contact_id_fk_contacts` (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `contactgroups`
+--
 
-CREATE TABLE `contactgroups` (
-  `contactgroup_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+CREATE TABLE IF NOT EXISTS `contactgroups` (
+  `contactgroup_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
   `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
   `del` tinyint(1) NOT NULL DEFAULT '0',
   `name` varchar(128) NOT NULL DEFAULT '',
-  PRIMARY KEY(`contactgroup_id`),
-  CONSTRAINT `user_id_fk_contactgroups` FOREIGN KEY (`user_id`)
-    REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  INDEX `contactgroups_user_index` (`user_id`,`del`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
-
-CREATE TABLE `contactgroupmembers` (
-  `contactgroup_id` int(10) UNSIGNED NOT NULL,
-  `contact_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-  PRIMARY KEY (`contactgroup_id`, `contact_id`),
-  CONSTRAINT `contactgroup_id_fk_contactgroups` FOREIGN KEY (`contactgroup_id`)
-    REFERENCES `contactgroups`(`contactgroup_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `contact_id_fk_contacts` FOREIGN KEY (`contact_id`)
-    REFERENCES `contacts`(`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) /*!40000 ENGINE=INNODB */;
+  PRIMARY KEY (`contactgroup_id`),
+  KEY `contactgroups_user_index` (`user_id`,`del`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL,
+  `firstname` varchar(128) NOT NULL DEFAULT '',
+  `surname` varchar(128) NOT NULL DEFAULT '',
+  `vcard` text,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`contact_id`),
+  KEY `user_contacts_index` (`user_id`,`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `event_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `start` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `end` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `summary` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `location` varchar(255) NOT NULL DEFAULT '',
+  `categories` varchar(255) NOT NULL DEFAULT '',
+  `all_day` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`event_id`),
+  KEY `user_id_fk_events` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+--
+-- Table structure for table `google_contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `google_contacts` (
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL,
+  `firstname` varchar(128) NOT NULL DEFAULT '',
+  `surname` varchar(128) NOT NULL DEFAULT '',
+  `vcard` text,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`contact_id`),
+  KEY `user_contacts_index` (`user_id`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `identities`
+--
 
-CREATE TABLE `identities` (
- `identity_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
- `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `del` tinyint(1) NOT NULL DEFAULT '0',
- `standard` tinyint(1) NOT NULL DEFAULT '0',
- `name` varchar(128) NOT NULL,
- `organization` varchar(128) NOT NULL DEFAULT '',
- `email` varchar(128) NOT NULL,
- `reply-to` varchar(128) NOT NULL DEFAULT '',
- `bcc` varchar(128) NOT NULL DEFAULT '',
- `signature` text,
- `html_signature` tinyint(1) NOT NULL DEFAULT '0',
- PRIMARY KEY(`identity_id`),
- CONSTRAINT `user_id_fk_identities` FOREIGN KEY (`user_id`)
-   REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
- INDEX `user_identities_index` (`user_id`, `del`)
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
+CREATE TABLE IF NOT EXISTS `identities` (
+  `identity_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `standard` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL,
+  `organization` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL,
+  `reply-to` varchar(128) NOT NULL DEFAULT '',
+  `bcc` varchar(128) NOT NULL DEFAULT '',
+  `signature` text,
+  `html_signature` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`identity_id`),
+  KEY `user_identities_index` (`user_id`,`del`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
+-- --------------------------------------------------------
 
-/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+--
+-- Table structure for table `messages`
+--
 
+CREATE TABLE IF NOT EXISTS `messages` (
+  `message_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `cache_key` varchar(128) CHARACTER SET ascii NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `idx` int(11) unsigned NOT NULL DEFAULT '0',
+  `uid` int(11) unsigned NOT NULL DEFAULT '0',
+  `subject` varchar(255) NOT NULL,
+  `from` varchar(255) NOT NULL,
+  `to` varchar(255) NOT NULL,
+  `cc` varchar(255) NOT NULL,
+  `date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `size` int(11) unsigned NOT NULL DEFAULT '0',
+  `headers` text NOT NULL,
+  `structure` text,
+  PRIMARY KEY (`message_id`),
+  UNIQUE KEY `uniqueness` (`user_id`,`cache_key`,`uid`),
+  KEY `created_index` (`created`),
+  KEY `index_index` (`user_id`,`cache_key`,`idx`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `sess_id` varchar(40) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `ip` varchar(40) NOT NULL,
+  `vars` mediumtext NOT NULL,
+  PRIMARY KEY (`sess_id`),
+  KEY `changed_index` (`changed`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `mail_host` varchar(128) NOT NULL,
+  `alias` varchar(128) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `last_login` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `language` varchar(5) DEFAULT NULL,
+  `preferences` text,
+  PRIMARY KEY (`user_id`),
+  KEY `username_index` (`username`),
+  KEY `alias_index` (`alias`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+--
+-- Constraints for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `cache`
+--
+ALTER TABLE `cache`
+  ADD CONSTRAINT `user_id_fk_cache` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contactgroupmembers`
+--
+ALTER TABLE `contactgroupmembers`
+  ADD CONSTRAINT `contactgroup_id_fk_contactgroups` FOREIGN KEY (`contactgroup_id`) REFERENCES `contactgroups` (`contactgroup_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contact_id_fk_contacts` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contactgroups`
+--
+ALTER TABLE `contactgroups`
+  ADD CONSTRAINT `user_id_fk_contactgroups` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `user_id_fk_contacts` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `user_id_fk_events` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `google_contacts`
+--
+ALTER TABLE `google_contacts`
+  ADD CONSTRAINT `google_contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `identities`
+--
+ALTER TABLE `identities`
+  ADD CONSTRAINT `user_id_fk_identities` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `user_id_fk_messages` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- SQL/sqlite.update.sql
+++
@@ -1,3 +0,0 @@
--- RoundCube Webmail update script for SQLite databases
--- Updates from version 1.0 on
-
